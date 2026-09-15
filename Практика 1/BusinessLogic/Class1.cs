7292d19df29432340b9469f88068ae8ac936be9b
Ноут <tomilovstas1@gmail.com>
2026-09-15 16:28:43 +0700

    model

--- Практика 1/BusinessLogic/Class1.cs
+++ Практика 1/BusinessLogic/Class1.cs
@@ -1,4 +1,4 @@
-﻿namespace Model
+﻿namespace BusinessLogic
 {
     public class Class1
     {
---
+++ Практика 1/Model/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public string Group { get; set; }
+        public Student(string name, string speciality, string group)
+        {
+            Name = name;
+            Speciality = speciality;
+            Group = group;
+        }
+    }
+}
